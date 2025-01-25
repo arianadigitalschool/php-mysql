8fa8429e9569ea5c58027c86950730c339ae011a
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2025-01-25 07:40:32 -0800

    first php file

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+// $school = "Digital School";
+// // $age = 22;
+
+// echo "I love $school";
+// // echo "My age is $age";
+
+
+// $x = 120;
+// $y = 20;
+
+// echo $x * $y
+
+
+$name = "Ariana";
+$surname = "Rexhepi";
+
+$fullName = $name . " " . $surname;
+
+echo $fullName;
+echo strlen($name);
+echo str_word_count($name);
+echo strrev($name);
+
+?>
