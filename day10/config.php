a43e51600ec5ea48c110a9d3c4ce635e9363413f
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2025-04-26 07:43:04 -0700

    form name added

---
+++ day10/config.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    $server = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'dbtest';
+
+    try{
+        $connect = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+
+    }catch(Exeption $e){
+        echo "Something went wrong";
+    }
+
+
+
+
+?>
---
+++ day10/register.php
@@ -0,0 +1,10 @@
+<?php
+
+    include_once('config.php');
+
+    if(isset($_POST['submit']))
+    {
+            $name = $_POST['name']
+    }
+
+?>
