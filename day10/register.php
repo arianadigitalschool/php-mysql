07ef31a5927c80570a28025ed1a354d130e099ef
arianadigitalschool <ariana.rexhepi@digitalschool.tech>
2025-05-03 07:48:17 -0700

    lohin/logout logic

--- day10/register.php
+++ day10/register.php
@@ -4,7 +4,40 @@
 
     if(isset($_POST['submit']))
     {
-            $name = $_POST['name']
+            $name = $_POST['name'];
+            $surname = $_POST['surname'];
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $tempPass = $_POST['password']; //endriti123 -> iuqwdrgbqwuidhquwduioq@#$%^&1235757
+            $password = password_hash($tempPass, PASSWORD_DEFAULT);
+
+    if(empty($name) || empty($surname) || empty($username) || empty($email) || empty($password)){
+        echo "You need to fill all these fields!";
+    }else{
+        $sql = "SELECT username FROM users WHERE username = :username";
+
+        $tempSQL = $conn->prepare($sql);
+        $tempSQL -> bindParam(':username, $username');
+        $tempSQL->execute();
+
+        if($tempSQL > 0){
+            echo "Username exists";
+            header("refresh:2, url=signupform.php");
+        }else{
+            $sql = "INSERT INTO users(name,surname,username, email, password) values(:name,:surname,:username, :email, :password)";
+            $insertSql = $conn -> prepare($sql);
+            $insertSql->bindParam(':name', $name);
+            $insertSql->bindParam(':surname', $surname);
+            $insertSql->bindParam(':username', $username);
+            $insertSql->bindParam(':email', $email);
+            $insertSql->bindParam(':password', $password);
+
+            $insertSql->execute()
+
+            echo "Data saved sucessfully!";
+            header("refresh:2, url=signin.php");
+        }
+    }
     }
 
 ?>
